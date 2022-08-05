188e76a10aeecc1c4da4a31b109ac30b6f88df8e
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2022-08-04 20:04:07 -0400

    rewrite

--- custom_components/purrsong/const.py
+++ custom_components/purrsong/const.py
@@ -1,3 +1,29 @@
-"""My platform constants"""
+""" Constants for PurrSong """
 
+import asyncio
+import logging
+
+from aiohttp.client_exceptions import ClientConnectionError
+from lavviebot.exceptions import LavviebotAuthError, LavviebotError
+
+from homeassistant.const import Platform
+
+LOGGER = logging.getLogger(__package__)
+
+DEFAULT_SCAN_INTERVAL = 60
 DOMAIN = "purrsong"
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.SENSOR,
+    Platform.UPDATE,
+]
+
+DEFAULT_NAME = "PurrSong"
+TIMEOUT = 8
+
+LAVVIEBOT_ERRORS = (
+    ClientConnectionError,
+    asyncio.TimeoutError,
+    LavviebotAuthError,
+    LavviebotError,
+)
